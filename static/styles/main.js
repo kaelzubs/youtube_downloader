0f3909c586010dc9637987540381e6af1121918c
kaelzubs <donmart4u@gmail.com>
2022-10-06 18:11:10 +0100

    Thursday commit 6

--- static/styles/main.js
+++ static/styles/main.js
@@ -4,7 +4,7 @@ document.onreadystatechange = function() {
         if (document.readyState !== "complete") {
             document.querySelector("#loading").style.visibility = "visible";
         } else {
-            document.querySelector("#loading").style.display = "none";
+            document.querySelector("#loading").style.visibility = "none";
         }
     }
 }
