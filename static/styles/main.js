c052d230d7bc98fd53c93e75262b6823b12c9606
kaelzubs <donmart4u@gmail.com>
2022-10-15 01:01:47 +0100

    Solved it today, celebrate 9

--- static/styles/main.js
+++ static/styles/main.js
@@ -1,8 +1,11 @@
 
 document.onreadystatechange = function() {
     window.onload = (e) => {
+        document.querySelector("#loading").style.visibility = "hidden"
         if (document.readyState !== "complete") {
             document.querySelector("#loading").style.visibility = "visible";
+        } else {
+            document.querySelector("#loading").style.visibility = "hidden"
         }
     }
 }
