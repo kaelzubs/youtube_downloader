b756f98470a81cb6dc5d3aea36262d7e56451097
kaelzubs <donmart4u@gmail.com>
2022-10-15 14:40:02 +0100

    Solved it today, celebrate 36

--- app.py
+++ app.py
@@ -26,6 +26,7 @@ Minify(app, html=True, js=True, cssless=True)
 
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
+app.config['CDN_TIMESTAMP'] = False
 
 CDN(app)
 ############################################################################################
@@ -81,7 +82,7 @@ def loading():
 
 
 ############################################################################################
-@app.endpoint('static')
+@app.template_global()
 def static(filename):
     static_url = app.config.get('STATIC_URL')
     if static_url:
