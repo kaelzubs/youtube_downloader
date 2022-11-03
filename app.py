57fb38d6f34d36d40007fd470091dd1720fa1d62
kaelzubs <donmart4u@gmail.com>
2022-11-03 21:57:30 +0000

    My commit 6

--- app.py
+++ app.py
@@ -16,6 +16,8 @@ from whitenoise import WhiteNoise
 
 ############################################################################################
 # configuring flask application
+os.environ['ENV'] == 'prod'
+
 app = Flask(
     __name__,
     static_url_path='', 
