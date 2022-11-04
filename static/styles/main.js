f81844121a79c0f104f63cee2fdeef14509581df
kaelzubs <donmart4u@gmail.com>
2022-11-04 05:52:23 +0000

    My commit 17

--- static/styles/main.js
+++
@@ -1,9 +0,0 @@
-document.onreadystatechange = function() {
-    window.onload = (e) => {
-        if (document.readyState !== "complete") {
-            document.querySelector('#loading').style.visibility = 'visible'
-        } else {
-            document.querySelector('#loading').remove()
-        }
-    }
-}
