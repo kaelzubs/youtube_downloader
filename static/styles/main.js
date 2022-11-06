1ee10edd261be478817fb2a2c54be4fe6e2a9277
kaelzubs <donmart4u@gmail.com>
2022-11-06 11:17:34 +0000

    commit me

--- static/styles/main.js
+++ static/styles/main.js
@@ -2,6 +2,8 @@ document.onreadystatechange = function() {
     window.onload = (e) => {
         if (document.readyState !== "complete") {
             document.getElementById('#loading').style.visibility = 'visible'
+        } else {
+            document.getElementById('#loading').style.visibility = 'hidden'
         }
     }
 }
