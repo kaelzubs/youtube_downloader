55b9474679c5924352ea3492eb8d823aec10faeb
kaelzubs <donmart4u@gmail.com>
2022-11-06 11:44:49 +0000

    commit me

--- static/styles/main.js
+++ static/styles/main.js
@@ -2,21 +2,22 @@ document.onreadystatechange = function() {
     window.onload = (e) => {
         if (document.readyState !== "complete") {
             document.getElementById('#loading').style.visibility = 'visible'
-        } else {
-            document.getElementById('#loading').style.visibility = 'hidden'
         }
     }
 }
 
+document.onreadystatechange = function() {
+    var fn = function () {
+        document.cookie = "cookie_consent=true";
+        document.getElementById('cookie-consent-container').hidden = true;
+    };
+    document.getElementById('cookie-consent').onclick = fn;
+}
 
-var fn = function () {
-    document.cookie = "cookie_consent=true";
-    document.getElementById('cookie-consent-container').hidden = true;
-};
-document.getElementById('cookie-consent').onclick = fn;
 
-
-//cache your selector
-setTimeout(function() {
-    document.querySelector("#cookie-consent-container").style.visibility = 'visible'
-}, 5000);
+document.onreadystatechange = function() {
+    //cache your selector
+    setTimeout(function() {
+        document.querySelector("#cookie-consent-container").style.visibility = 'visible'
+    }, 5000);
+}
