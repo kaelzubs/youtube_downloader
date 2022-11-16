a92e4c61b37275310300870788cf6f7124342688
kaelzubs <donmart4u@gmail.com>
2022-11-16 04:51:07 +0000

    commit me

--- static/styles/clipboard.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let pasteButton = document.getElementsByTagName('button')[0];
-    pasteButton.addEventListener('click', function () {
-        navigator.clipboard
-        .readText()
-        .then(cliptext => (document.getElementById('clipboard-paste').innerText = cliptext), err => console.log(err));
-    });
-});
