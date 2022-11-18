c04c13af19408a0e03d564935b9915d0f58358f5
kaelzubs <donmart4u@gmail.com>
2022-11-18 20:55:13 +0000

    commit me

--- static/styles/delay.js
+++ static/styles/delay.js
@@ -1,6 +1,9 @@
-setTimeout(() => {
-    if (document.getElementById('cookie-consent-container')) {
-        document.getElementById('cookie-consent-container').hidden = false;
-    }
-}, 7);
+(function(){
+    setTimeout(() => {
+        var delay_func = document.getElementById('cookie-consent-container')
+        if (delay_func) {
+            delay_func.hidden = false;
+        }
+    }, 7);
+});
 
