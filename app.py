40d50f7d8f2a3128494cf33aa771683743cac5cc
kaelzubs <donmart4u@gmail.com>
2022-12-04 01:56:22 +0100

    commit me

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ def pretty_size(bytes, units=UNITS_MAPPING):
 ############################################################################################
 @app.after_request
 def add_security_headers(resp):
-    resp.headers["Content-Security-Policy"]="default-src * self blob: data: gap:; style-src * self 'unsafe-inline' blob: data: gap:; script-src * 'self' 'unsafe-eval' 'unsafe-inline' blob: data: gap:; object-src * 'self' blob: data: gap:; img-src * self 'unsafe-inline' blob: data: gap:; connect-src self * 'unsafe-inline' blob: data: gap:; frame-src * self blob: data: gap:;"
+    resp.headers['Content-Security-Policy']='default-src \'self\''
     return resp
 ############################################################################################
 
