d91d56056241426e5b3aa216c33f827368605f5a
kaelzubs <donmart4u@gmail.com>
2022-12-08 13:09:55 +0100

    another commit begins

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ def pretty_size(bytes, units=UNITS_MAPPING):
 ############################################################################################
 @app.after_request
 def add_security_headers(resp):
-    resp.headers['Content-Security-Policy']="default-src * 'unsafe-inline'; img-src * data:; script-src * data: 'unsafe-inline'; style-src-elem * data:; object-src 'none'; media-src * data:; font-src * data:; frame-src * data:; connect-src * data:;"
+    resp.headers['Content-Security-Policy']="default-src * 'unsafe-inline'; img-src * data:; script-src * 'strict-dynamic'; style-src-elem * data:; object-src 'none'; media-src * data:; font-src * data:; frame-src * data:; connect-src * data:;"
     return resp
 ############################################################################################
 
