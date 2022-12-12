3c75a650108dd862dd3f31ae5da91d6ff8896515
kaelzubs <donmart4u@gmail.com>
2022-12-12 04:03:00 +0100

    My Commit

--- app.py
+++ app.py
@@ -296,15 +296,15 @@ def inject_template_scope():
 
 
 
-############################################################################################
-@app.before_request
-def before_request():
-    scheme = request.headers.get('X-Forwarded-Proto')
-    if scheme and scheme == 'http' and request.url.startswith('http://'):
-        url = request.url.replace('http://', 'https://', 1)
-        code = 301
-        return redirect(url, code=code)
-############################################################################################
+# ############################################################################################
+# @app.before_request
+# def before_request():
+#     scheme = request.headers.get('X-Forwarded-Proto')
+#     if scheme and scheme == 'http' and request.url.startswith('http://'):
+#         url = request.url.replace('http://', 'https://', 1)
+#         code = 301
+#         return redirect(url, code=code)
+# ############################################################################################
 
 
 
