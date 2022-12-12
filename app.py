001b472a62f34bac1e3af81ec3027b446886dc1b
kaelzubs <donmart4u@gmail.com>
2022-12-12 10:16:02 +0100

    My Commit

--- app.py
+++ app.py
@@ -296,18 +296,6 @@ def inject_template_scope():
 
 
 
-# ############################################################################################
-# @app.before_request
-# def before_request():
-#     scheme = request.headers.get('X-Forwarded-Proto')
-#     if scheme and scheme == 'http' and request.url.startswith('http://'):
-#         url = request.url.replace('http://', 'https://', 1)
-#         code = 301
-#         return redirect(url, code=code)
-# ############################################################################################
-
-
-
 ############################################################################################
 # running flask server
 if __name__ == "__main__":
