08ef1725949de6d96e52d396667c9b09a7eacff3
kaelzubs <donmart4u@gmail.com>
2022-12-13 08:49:26 +0100

    Commited

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ Minify(app=app, html=True, js=True, cssless=True)
 app.config.from_mapping(config)
 Cache(app)
 
-SSLify(app, subdomains=True, permanent=True)
+SSLify(app)
 ############################################################################################
 
 
