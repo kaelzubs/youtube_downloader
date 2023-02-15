c0d0644367a72ed6c1588ea8149260b4e82cdaee
Elvis chukwuka <37744693+kaelzubs@users.noreply.github.com>
2023-02-16 00:49:28 +0100

    Create delay.js

---
+++ static/styles/delay.js
@@ -0,0 +1,6 @@
+setTimeout(() => {
+    var delay_func = document.getElementById('cookie-consent-container')
+    if (delay_func) {
+        delay_func.style.visibility = 'visible';
+    }
+}, 7000)
