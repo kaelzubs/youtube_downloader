bd337ab05d9a17bbceed8623caa375b601d3c617
Elvis chukwuka <37744693+kaelzubs@users.noreply.github.com>
2023-02-15 22:44:03 +0100

    Delete delay.js

--- static/styles/delay.js
+++
@@ -1,7 +0,0 @@
-setTimeout(() => {
-    var delay_func = document.getElementById('cookie-consent-container')
-    if (delay_func) {
-        delay_func.style.visibility = 'visible';
-    }
-}, 7000);
-
